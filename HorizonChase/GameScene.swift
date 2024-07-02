6bdc3789fead00485dccee19ce6b6c20bdb0b2a0
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2024-07-02 10:14:30 +0300

    ADD racingCar Sprite

--- HorizonChase/GameScene.swift
+++ HorizonChase/GameScene.swift
@@ -10,7 +10,17 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let screenSize: CGRect = UIScreen.main.bounds
+    
+    
+    
     override func didMove(to view: SKView) {
+        backgroundColor = UIColor(named: "asphaltColor") ?? UIColor.gray
         
+        let racingCar = SKSpriteNode(imageNamed: "racingCar")
+        racingCar.setScale(0.2)
+        racingCar.position = CGPoint(x: frame.midX, y: frame.minX + 100)
+
+        addChild(racingCar)
     }
 }
